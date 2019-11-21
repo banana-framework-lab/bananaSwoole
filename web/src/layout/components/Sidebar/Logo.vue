3676945bc833e7c93eaeae1ddba0d9b714f10fe7
张中昊 <335825098@qq.com>
2019-11-21 15:19:16 +0800

    U 更新细节

--- web/src/layout/components/Sidebar/Logo.vue
+++ web/src/layout/components/Sidebar/Logo.vue
@@ -3,7 +3,7 @@
     <transition name="sidebarLogoFade">
       <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
         <!-- <img v-if="logo" :src="logo" class="sidebar-logo"> -->
-        <h1 class="sidebar-title">{{ title }} </h1>
+        <h1 class="sidebar-title"> sva </h1>
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
         <!-- <img v-if="logo" :src="logo" class="sidebar-logo"> -->
