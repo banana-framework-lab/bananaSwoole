e377d00642f5774d0e6328c312ac9f350eb1b549
张中昊 <zhangzhonghao@tanwan.com>
2020-03-13 09:16:50 +0800

    A httpCURL

--- library/Virtual/Model/HttpModel/AbstractHttpModel.php
+++ library/Virtual/Model/HttpModel/AbstractHttpModel.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Redis的Model抽象类
+ * Http的Model抽象类
  * User: zzh
  * Date: 2018/10/10
  * Time: 17:28
@@ -8,15 +8,50 @@
 
 namespace Library\Virtual\Model\HttpModel;
 
+use Library\Entity\Swoole\EntitySwooleServer;
+use Swoole\Http\Client;
+
 /**
  * Class AbstractRedisModel
  * @package Library\Virtual\Model\HttpModel
  */
 abstract class AbstractHttpModel
 {
+    /**
+     * 成功的请求码
+     * @var int $successCode
+     */
     public static $successCode = 1;
 
+    /**
+     * 失败的请求码
+     * @var int $failCode
+     */
     public static $failCode = 0;
+
+    /**
+     * 获取协程版本的http请求数据
+     * @param string $url
+     * @return array
+     */
+    private function getUrlData(string $url)
+    {
+        $data = [];
+        if (mb_strpos('http://', $url)) {
+            $url = str_replace('http://', '', $url);
+            $data['port'] = 80;
+            $data['domain'] = (explode('/', $url))[0];
+            $data['path'] = str_replace($data['domain'], '', $url);
+        } elseif (mb_strpos('https://', $url)) {
+            $url = str_replace('https://', '', $url);
+            $data['port'] = 443;
+            $data['domain'] = (explode('/', $url))[0];
+            $data['path'] = str_replace($data['domain'], '', $url);
+        } else {
+            return [];
+        }
+        return $data;
+    }
 
     /**
      * @param $url
@@ -24,19 +59,29 @@ abstract class AbstractHttpModel
      * @param int $timeout
      * @return string
      */
-    function postCurl($url, $postData, $timeout = 5)
+    function postCurl(string $url, $postData, $timeout = 5)
     {
-        $ch = curl_init();  //初始化curl
-        curl_setopt($ch, CURLOPT_URL, $url);  //抓取指定网页
-        curl_setopt($ch, CURLOPT_HEADER, 0);  //设置header
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  //设置不输出直接返回字符串
-        curl_setopt($ch, CURLOPT_POST, 1);  //post提交方式
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
-        curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . "/../../../Cert/Curl/cacert.pem");  //设置CA证书
-        $result = curl_exec($ch);  //运行curl
-        curl_close($ch);
-
+        if (EntitySwooleServer::getInstance()) {
+            $requestData = $this->getUrlData($url);
+            if (!$requestData) {
+                return false;
+            }
+            $client = new Client($requestData['domain'], $requestData['port']);
+            $client->set(['timeout' => $timeout]);
+            $client->post($requestData['path'], $postData, null);
+            $result = $client->body;
+        } else {
+            $ch = curl_init();  //初始化curl
+            curl_setopt($ch, CURLOPT_URL, $url);  //抓取指定网页
+            curl_setopt($ch, CURLOPT_HEADER, 0);  //设置header
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  //设置不输出直接返回字符串
+            curl_setopt($ch, CURLOPT_POST, 1);  //post提交方式
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+            curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+            curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . "/../../../Cert/Curl/cacert.pem");  //设置CA证书
+            $result = curl_exec($ch);  //运行curl
+            curl_close($ch);
+        }
         return $result;
     }
 
@@ -47,14 +92,25 @@ abstract class AbstractHttpModel
      */
     function getCurl($url, $timeout = 5)
     {
-        $ch = curl_init();  //初始化curl
-        curl_setopt($ch, CURLOPT_URL, $url);  //抓取指定网页
-        curl_setopt($ch, CURLOPT_HEADER, 0);  //设置header
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  //设置不输出直接返回字符串
-        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
-        curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . "/../../../Cert/Curl/cacert.pem");  //设置CA证书
-        $result = curl_exec($ch);  //运行curl
-        curl_close($ch);
+        if (EntitySwooleServer::getInstance()) {
+            $requestData = $this->getUrlData($url);
+            if (!$requestData) {
+                return false;
+            }
+            $client = new Client($requestData['domain'], $requestData['port']);
+            $client->set(['timeout' => $timeout]);
+            $client->get($requestData['path'], null);
+            $result = $client->body;
+        } else {
+            $ch = curl_init();  //初始化curl
+            curl_setopt($ch, CURLOPT_URL, $url);  //抓取指定网页
+            curl_setopt($ch, CURLOPT_HEADER, 0);  //设置header
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  //设置不输出直接返回字符串
+            curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+            curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . "/../../../Cert/Curl/cacert.pem");  //设置CA证书
+            $result = curl_exec($ch);  //运行curl
+            curl_close($ch);
+        }
 
         return $result;
     }
