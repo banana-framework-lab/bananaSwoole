b0b146c18c965f5c0612332e350576503254698d
张中昊 <zhangzhonghao@tanwan.com>
2021-10-13 17:21:55 +0800

    D 删除过度复用的东西

--- library/Server/BananaSwooleServer.php
+++ library/Server/BananaSwooleServer.php
@@ -2,8 +2,12 @@
 
 namespace Library\Server;
 
+use Closure;
 use Library\Container;
 use Library\Virtual\Server\AbstractSwooleServer;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use SplFileInfo;
 use Swoole\Coroutine;
 use Swoole\Server\Task;
 use Swoole\Table;
@@ -12,14 +16,193 @@ use Swoole\WebSocket\Frame;
 use Swoole\WebSocket\Server;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
+use Swoole\WebSocket\Server as SwooleWebSocketServer;
 use Throwable;
 
 /**
  * Class SwooleWebSocketServer
  * @package Library\Server
  */
-class BananaSwooleServer extends BaseSwooleServer
+class BananaSwooleServer
 {
+
+    /**
+     * @var SwooleWebSocketServer $server
+     */
+    protected $server;
+
+    /**
+     * @var AbstractSwooleServer $appServer
+     */
+    protected $appServer;
+
+    /**
+     * @var int $port
+     */
+    protected $port;
+
+    /**
+     * @var int $workerNum
+     */
+    protected $workerNum;
+
+    /**
+     * @var int $taskNum
+     */
+    protected $taskNum;
+
+    /**
+     * @var array $appServerList
+     */
+    protected $appServerList;
+
+    /**
+     * @var Table $bindTable
+     */
+    protected $bindTable;
+
+    /**
+     * @var Table $reloadTable
+     */
+    protected $reloadTable;
+
+    /**
+     * @var int $reloadTickId
+     */
+    protected $reloadTickId;
+
+    /**
+     * @var bool $isFirstStart
+     */
+    protected $isFirstStart = true;
+
+    /**
+     * @var string $startDateTime
+     */
+    protected $startDateTime;
+
+    /**
+     * @var string $serverConfigIndex
+     */
+    protected $serverConfigIndex = 'index';
+
+    /**
+     * @var int $echoWidth
+     */
+    protected $echoWidth = 75;
+
+    /**
+     * @var int $cliParamNumber
+     */
+    protected $cliParamNumber;
+
+    /**
+     * @var array $cliParamData
+     */
+    protected $cliParamData;
+
+    /**
+     * @var string $serverName
+     */
+    protected $serverName;
+
+    /**
+     * worker启动完成后报的程序信息
+     * @param string $serverType
+     * @param string $xChar
+     * @param string $yChar
+     * @param int $echoWidth
+     */
+    protected function startEcho(string $serverType = "SwooleServer", string $xChar = '-', string $yChar = '|', int $echoWidth = 75)
+    {
+        $this->echoWidth = $echoWidth;
+
+        $logo = BananaSwoole(true, 'array');
+        $this->startDateTime = date('Y-m-d H:i:s');
+
+        echo "\n";
+        foreach ($logo as $key => $value) {
+            echo ' ' . str_pad("{$value}", $echoWidth - 2, ' ', STR_PAD_BOTH) . " \n";
+        }
+        echo "\n";
+        echo str_pad("", $echoWidth, $xChar, STR_PAD_BOTH) . "\n";
+        echo $yChar . str_pad("$serverType start", $echoWidth - 2, ' ', STR_PAD_BOTH) . "$yChar\n";
+        echo str_pad("", $echoWidth, $xChar, STR_PAD_BOTH) . "\n";
+        echo $yChar . str_pad("", $echoWidth - 2, ' ', STR_PAD_BOTH) . "$yChar\n";
+        echo $yChar . str_pad("listen_ip: 0.0.0.0  listen_port: {$this->port}  address: //0.0.0.0:{$this->port}", $echoWidth - 2, ' ', STR_PAD_BOTH) . "$yChar\n";
+        echo $yChar . str_pad("", $echoWidth - 2, ' ', STR_PAD_BOTH) . "$yChar\n";
+        echo $yChar . str_pad("manage_pid: {$this->server->manager_pid}      master_pid: {$this->server->master_pid}      worker_number: {$this->workerNum}", $echoWidth - 2, ' ', STR_PAD_BOTH) . "$yChar\n";
+        echo $yChar . str_pad("", $echoWidth - 2, ' ', STR_PAD_BOTH) . "$yChar\n";
+        echo $yChar . str_pad("autoHotReloadId: {$this->reloadTickId}   task_number: {$this->taskNum}   time: {$this->startDateTime}", $echoWidth - 2, ' ', STR_PAD_BOTH) . "$yChar\n";
+        echo $yChar . str_pad("", $echoWidth - 2, ' ', STR_PAD_BOTH) . "$yChar\n";
+        echo str_pad("", $echoWidth, $xChar, STR_PAD_BOTH) . "\n";
+        echo "\n";
+    }
+
+
+    /**
+     * worker启动完成后开启自动热加载
+     * @return Closure
+     */
+    protected function autoHotReload()
+    {
+        return function () {
+            // 读取需要热加载的路径
+            $pathList = Container::getConfig()->get('reload.path_list', []);
+            $isReload = false;
+            $iNodeList = [];
+
+            //判断文件更新或者新增
+            foreach ($pathList as $pathKey => $pathValue) {
+                $dirIterator = new RecursiveDirectoryIterator($pathValue);
+                $iterator = new RecursiveIteratorIterator($dirIterator);
+
+                /* @var SplFileInfo $fileValue */
+                foreach ($iterator as $fileKey => $fileValue) {
+                    $ext = $fileValue->getExtension();
+                    if ($ext == 'php') {
+                        $iNode = $fileValue->getInode();
+                        $mTime = $fileValue->getMTime();
+                        $iNodeList[] = $iNode;
+                        if ($this->reloadTable->exist($iNode)) {
+                            if ($this->reloadTable->get($iNode)['mTime'] != $mTime) {
+                                $this->reloadTable->set($iNode, [
+                                    'mTime' => $mTime
+                                ]);
+                                $isReload = true;
+                            }
+                        } else {
+                            $this->reloadTable->set($iNode, [
+                                'mTime' => $mTime
+                            ]);
+                            $isReload = true;
+                        }
+                    }
+                }
+            }
+
+            //判断文件删除
+            foreach ($this->reloadTable as $reloadKey => $reloadValue) {
+                if (!in_array((int)$reloadKey, $iNodeList)) {
+                    $this->reloadTable->del($reloadKey);
+                    $isReload = true;
+                }
+            }
+
+            if (!$this->isFirstStart) {
+                if ($isReload) {
+                    $this->server->reload();
+                }
+            } else {
+                $this->isFirstStart = false;
+            }
+        };
+    }
+
+    /**
+     * BananaSwooleServer constructor.
+     * @param string $serverConfigIndex
+     */
     public function __construct(string $serverConfigIndex = 'index')
     {
         $this->serverConfigIndex = $serverConfigIndex;
@@ -27,7 +210,18 @@ class BananaSwooleServer extends BaseSwooleServer
         Container::setConfig();
         Container::getConfig()->initSwooleConfig();
         Container::setSwooleSever($serverConfigIndex);
-        parent::__construct();
+
+        global $argc;
+        $this->cliParamNumber = $argc;
+
+        global $argv;
+        $this->cliParamData = $argv;
+
+        if ($this->cliParamData[0] != 'bananaSwoole') {
+            $this->serverName = str_replace('.php', '', $this->cliParamData[0]);
+        } else {
+            $this->serverName = $this->cliParamData[2];
+        }
     }
 
     /**
