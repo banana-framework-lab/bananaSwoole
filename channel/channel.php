b9ce40028066d09be20d4a29dc93c20c09f40f66
张中昊 <zhangzhonghao@tanwan.com>
2021-11-01 15:08:05 +0800

    U 更新gitnore

--- channel/channel.php
+++ channel/channel.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2019/10/28
- * Time: 20:25
- */
+
 return [
     'Api' => '\App\Api\Handler\TestHandler'
 ];
--- route/route.php
+++ route/route.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2019/9/5 0005
- * Time: 17:05
- */
 
 return [
     'test/index' => '\App\Api\Controller\TestController@index',
